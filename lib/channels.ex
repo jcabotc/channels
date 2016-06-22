d54d2d93979729abb0b26c6a3740a034f2b6cd18
Jaime Cabot Campins <jcabot@gmail.com>
2016-06-22 17:18:46 +0200

    logging configuration

--- lib/channels.ex
+++ lib/channels.ex
@@ -1,9 +1,15 @@
 defmodule Channels do
   use Application
+  require Logger
 
   @channels_sup Channels.Supervisor
 
   def start(_type, _args) do
+    Logger.info("========== CONFIG ==========")
+    Logger.info(inspect Application.get_all_env(:channels))
+    Logger.info(inspect Channels.Config.conn_configs)
+    Logger.info("============================")
+
     Channels.Supervisor.start_link(name: @channels_sup)
   end
 end
